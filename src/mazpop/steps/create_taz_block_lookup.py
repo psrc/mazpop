1f9d4b21f8bba09ee210b6b4e2a1a6e551672100
jkolberg <jwkolberg@gmail.com>
2026-09-23 17:03:05 -0700

    add taz block lookup step

--- src/mazpop/steps/create_taz_block_lookup.py
+++ src/mazpop/steps/create_taz_block_lookup.py
@@ -5,18 +5,27 @@ from mazpop.util.pipeline import Pipeline
 
 
 def load_taz_layer(pipeline):
-    taz_path = Path.joinpath(pipeline.get_data_dir(), pipeline.settings['taz_shapefile'])
-    return gpd.read_file(taz_path)
+    taz_path = Path(pipeline.get_data_dir()) / pipeline.settings['taz_layer']
+    taz = gpd.read_file(taz_path)
+    taz = taz.rename(columns={pipeline.settings['taz_id_field']: 'zone_id'})
+    taz =taz.to_crs(epsg=4326)
+    return taz[['zone_id', 'geometry']]
 
 
 def download_blocks(year,state_str, county_ids):
     year_2_digit = str(year)[-2:]
-    # Example URL format for reference:
+    if year == 2020:
+        url_year_2_digit = year_2_digit
+    else:
+        url_year_2_digit = ''
+    # Example 2020 URL format for reference:
     # https://www2.census.gov/geo/tiger/TIGER2020/TABBLOCK20/tl_2020_53_tabblock20.zip
+    # Example 2010 URL format for reference:
+    # https://www2.census.gov/geo/tiger/TIGER2020/TABBLOCK/tl_2020_53_tabblock10.zip
     url = (
-        f"https://www2.census.gov/geo/tiger/TIGER{year}/"
-        f"TABBLOCK{year_2_digit}/"
-        f"tl_{year}_{state_str}_tabblock{year_2_digit}.zip"
+        f"https://www2.census.gov/geo/tiger/TIGER2020/"
+        f"TABBLOCK{url_year_2_digit}/"
+        f"tl_2020_{state_str}_tabblock{year_2_digit}.zip"
     )
     print(f"Downloading {year} blocks from {url}")
 
@@ -29,6 +38,7 @@ def download_blocks(year,state_str, county_ids):
             county_id=lambda df: ('1' + df[geoid].str[:5]).astype(int),
         )
         .query('county_id.isin(@county_ids)')
+        .to_crs(epsg=4326)
         [['block_id', 'geometry']]
     )
 
